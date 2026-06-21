ed192262d5fcd12ffea52faa28e99e90ee58bb17
Sara Persson <sara.persson.82@gmail.com>
2026-06-21 17:22:24 +0200

    build(backend): add PostgreSQL JPA foundation (#4)

--- backend/src/test/java/io/github/sarapersson/streamparty/StreamPartyPlannerBackendApplicationTests.java
+++ backend/src/test/java/io/github/sarapersson/streamparty/StreamPartyPlannerBackendApplicationTests.java
@@ -2,12 +2,29 @@ package io.github.sarapersson.streamparty;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@Import(StreamPartyPlannerBackendApplicationTests.TestcontainersConfiguration.class)
 class StreamPartyPlannerBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@TestConfiguration(proxyBeanMethods = false)
+	static class TestcontainersConfiguration {
+
+		@Bean
+		@ServiceConnection
+		PostgreSQLContainer postgresContainer() {
+			return new PostgreSQLContainer("postgres:18");
+		}
+
 	}
 
 }
