0aeca86feb32116dd49234822f67239932b7f3a9
Sara Persson <sara.persson.82@gmail.com>
2026-06-21 11:50:53 +0200

    feat(backend): add Spring Boot skeleton (#1)

---
+++ backend/src/main/java/io/github/sarapersson/streamparty/StreamPartyPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.sarapersson.streamparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamPartyPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamPartyPlannerBackendApplication.class, args);
+	}
+
+}
